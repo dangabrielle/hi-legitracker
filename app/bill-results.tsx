abe47ef17d69310d20a9ecf08fc731f7dc1cda3e
Gabrielle <hoangabrielle@gmail.com>
2025-02-05 17:38:56 -1000

    format individual bills

--- app/bill-results.tsx
+++ app/bill-results.tsx
@@ -1,45 +1,140 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
 
 const BillResults = ({ results }) => {
   const [billArrangement, setBillArrangement] = useState(results);
-
+  const [query, setQuery] = useState("");
   const sortbydate = (results) => {
     const testing = Object.values(results).sort(
       (a, b) => new Date(b.last_action_date) - new Date(a.last_action_date)
     );
-    console.log(testing);
+
     setBillArrangement(testing);
   };
 
+  const sortByRelevance = (results) => {
+    const billsByRelevance = Object.values(results).sort(
+      (a, b) => b.relevance - a.relevance
+    );
+
+    setBillArrangement(billsByRelevance);
+  };
+  const sortByBillNum = (results) => {
+    const billsByBillNum = Object.values(results).sort(
+      (a, b) => a.bill_id - b.bill_id
+    );
+    console.log(billsByBillNum);
+    setBillArrangement(billsByBillNum);
+  };
+
+  const handleQuery = (e: any) => {
+    const filteredBills = Object.values(results).filter((bill) =>
+      bill?.title?.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+    console.log(filteredBills);
+    setBillArrangement(filteredBills);
+  };
+
   return (
-    <div>
-      <div className="h-screen md:m-20">
-        <h1>Hawaii Legislation Tracker</h1>
-        <h1>Economics Results</h1>
-        <button onClick={() => sortbydate(results)}>sort</button>
-        {/* <h1>results: {Object.keys(bill).length - 1}</h1> */}
-        <main className="size-full flex flex-col lg:grid lg:grid-cols-3  ">
-          {Object.entries(billArrangement).map(
-            ([key, val]) =>
-              key !== "summary" && (
-                <div
-                  key={key}
-                  className=" bg-slate-100 text-xs md:text-sm gap-y-3 rounded-lg items-start justify-start text-left p-5 flex flex-col m-3 "
-                >
-                  <h1 className="font-bold">{val.title}</h1>
-                  <p>Last action date: {val.last_action_date}</p>
-                  <p>Bill Number: {val.bill_number}</p>
-                  <p>Bill ID: {val.bill_id}</p>
-                  <Link href={`${val.text_url}`}>View bill</Link>
-                  <Link href={`${val.research_url}`}>View research URL</Link>
-                  <p>Last action: {val.last_action}</p>
-                </div>
-              )
-          )}
-        </main>
+    <div className="h-screen flex flex-col md:m-10">
+      <header className="mt-14 w-full gap-y-5  flex flex-col items-center justify-center">
+        <Image
+          src={"/UHEROLogo-Color_HighRes.png"}
+          height={250}
+          width={250}
+          alt="logo"
+        />
+        <h1 className="md:text-3xl text-xl mb-10 font-bold">
+          Hawaii Legislation Bill Tracker
+        </h1>
+      </header>
+      <div className="w-full h-fit mb-10 flex flex-col items-center justify-center gap-x-5">
+        <div className="flex w-full h-fit justify-center gap-x-5">
+          <Button variant="outline" onClick={() => sortbydate(results)}>
+            View latest
+          </Button>
+          <Button variant="outline" onClick={() => sortByRelevance(results)}>
+            View by Relevance
+          </Button>
+          <Button variant="outline" onClick={() => sortByBillNum(results)}>
+            View by Bill ID
+          </Button>
+        </div>
+        <div className="w-full max-w-xl md:w-1/2 px-10 pt-5">
+          <Label className="pl-1 text-gray-500 font-semibold" htmlFor="search">
+            Search current bills:
+          </Label>
+          <Input
+            type="search"
+            onChange={(e) => handleQuery(e)}
+            placeholder="Search"
+          />
+        </div>
       </div>
+      {/* <h1>results: {Object.keys(bill).length - 1}</h1> */}
+      <main className="h-fit w-full pb-10 flex flex-col lg:grid lg:grid-cols-3  ">
+        {Object.entries(billArrangement).map(
+          ([key, val]) =>
+            key !== "summary" && (
+              <div
+                key={key}
+                className="hover:scale-105 transition-transform ease-in-out delay-100 bg-slate-100 text-xs md:text-sm gap-y-3 rounded-lg items-start justify-start text-left p-5 flex flex-col m-3 "
+              >
+                <div className="flex w-full space-x-5 justify-between">
+                  <h1 className="font-bold">{val.title}</h1>
+                  <div className="right-0 top-0 flex gap-x-1">
+                    <Button variant="outline">
+                      <Link
+                        href={`${val.text_url}`}
+                        className="font-bold text-xs text-cyan-900 rounded-lg"
+                        target="_blank"
+                      >
+                        View bill
+                      </Link>
+                    </Button>
+                    <Button variant="outline">
+                      <Link
+                        className="font-bold text-xs text-cyan-900  rounded-lg"
+                        href={`${val.research_url}`}
+                        target="_blank"
+                      >
+                        Info
+                      </Link>
+                    </Button>
+                  </div>
+                </div>
+                <p>
+                  <span className="font-bold text-gray-500">Relevance:</span>
+                  {val.relevance}
+                </p>
+                <p>
+                  <span className="font-bold text-gray-500">
+                    Last action date:
+                  </span>{" "}
+                  {val.last_action_date}
+                </p>
+                <p>
+                  <span className="font-bold text-gray-500">Bill Number: </span>
+                  {val.bill_number}
+                </p>
+                <p>
+                  <span className="font-bold text-gray-500">Bill ID: </span>{" "}
+                  {val.bill_id}
+                </p>
+
+                <p>
+                  <span className="font-bold text-gray-500">Last action:</span>{" "}
+                  {val.last_action}
+                </p>
+              </div>
+            )
+        )}
+      </main>
     </div>
   );
 };
