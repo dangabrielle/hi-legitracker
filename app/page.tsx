9883f74bea423477755be4776b1698a91d674972
Gabrielle <hoangabrielle@gmail.com>
2025-02-11 17:38:36 -1000

    organize outputs, track specific changes

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,24 @@ type BillType = {
   last_action_date: string;
   last_action: string;
   title: string;
+  created_at: Date;
 };
+
+type UpdatedBillFields = {
+  bill_id: number;
+  bill_number: string;
+  relevance?: string;
+  state?: string;
+  change_hash: string;
+  url: string;
+  text_url?: string;
+  research_url?: string;
+  last_action_date?: string;
+  last_action?: string;
+  title?: string;
+};
+
+type NewBill = [bill_number: string, url: string];
 
 const legiscanKey = process.env.LEGISCAN_KEY;
 const sql = neon(`${process.env.DATABASE_URL}`);
@@ -25,7 +42,7 @@ const sql = neon(`${process.env.DATABASE_URL}`);
 async function getData() {
   try {
     const response = await sql`SELECT * FROM bills`;
-    return response;
+    return response as BillType[];
   } catch (error) {
     console.error("Error fetching bills from db", error);
     return [];
@@ -34,8 +51,8 @@ async function getData() {
 
 async function handleBill(
   bill: BillType,
-  newBills: string[],
-  updatedBills: string[]
+  newBills: NewBill[],
+  updatedBills: UpdatedBillFields[]
 ) {
   const {
     bill_id,
@@ -53,25 +70,44 @@ async function handleBill(
 
   try {
     // check if bill exists in db
-    const storedBill =
+    const billStatus =
       await sql`SELECT EXISTS (SELECT 1 FROM bills WHERE (bill_number = ${bill_number}) AND bill_id = ${bill_id}) AS exists;`;
-
+    console.log(billStatus);
     // if exists, check if hash changed + update as necessary
     // else add it to the db
-    if (storedBill[0].exists) {
+    if (billStatus[0].exists) {
       const response =
-        await sql`SELECT change_hash FROM bills WHERE (bill_number = ${bill_number}) AND bill_id = ${bill_id}`;
-      const dB_hash = response[0].change_hash;
+        await sql`SELECT * FROM bills WHERE (bill_number = ${bill_number}) AND bill_id = ${bill_id}`;
+      const dbBill = response[0];
+      const dB_hash = dbBill.change_hash;
 
-      if (bill.change_hash !== dB_hash) {
+      if (change_hash !== dB_hash) {
+        // for UI purposes, distinguishes what fields have changed
+        updatedBills.push({
+          bill_id: bill_id,
+          bill_number: bill_number,
+          relevance: relevance !== dbBill.relevance ? relevance.toString() : "",
+
+          change_hash: change_hash,
+          url: url,
+          text_url: text_url !== dbBill.text_url ? text_url : "",
+          research_url:
+            research_url !== dbBill.research_url ? research_url : "",
+          last_action_date:
+            last_action_date !== dbBill.last_action_date
+              ? last_action_date
+              : "",
+          last_action: last_action !== dbBill.last_action ? last_action : "",
+          title: title !== dbBill.title ? title : "",
+        });
+        // updates bill accordingly
         await sql`UPDATE bills SET change_hash = ${change_hash}, last_action=${last_action}, last_action_date=${last_action_date}, relevance=${relevance}, research_url=${research_url}, state=${state}, text_url=${text_url}, title=${title}, url=${url} WHERE (bill_number = ${bill_number}) AND bill_id = ${bill_id}`;
-        updatedBills.push(bill_number);
         console.log("hash changed");
       }
     } else {
       await sql`INSERT INTO bills (bill_id, bill_number, change_hash, last_action, last_action_date, relevance, research_url, state, text_url, title, url) VALUES (${bill_id}, ${bill_number}, ${change_hash}, ${last_action}, ${last_action_date}, ${relevance}, ${research_url}, ${state},  ${text_url}, ${title}, ${url})`;
 
-      newBills.push(bill_number);
+      newBills.push([bill_number, url]);
     }
   } catch (error) {
     console.error(error);
@@ -83,13 +119,14 @@ async function handleLegiscanData() {
     // called every 24 hrs
     const data = await fetch(
       `https://api.legiscan.com/?key=${legiscanKey}&op=getSearch&state=HI&query=UHERO+OR+"task+force"+OR+"economic+research+organization"+OR+"working+group"`,
-      { cache: "force-cache", next: { revalidate: 86400 } }
+      { next: { revalidate: 86400 } }
     );
 
     if (data.ok) {
       const results = await data.json();
+
       const bills = await results.searchresult;
-      console.log("bills" + bills);
+
       // clean up response data, reformat to an array
       const billsArray: BillType[] = Object.values(bills);
 
@@ -103,48 +140,21 @@ async function handleLegiscanData() {
 }
 
 export default async function Home() {
-  const newBills: string[] = [];
-  const updatedBills: string[] = [];
-
-  // try {
-  //   // called every 24 hrs
-  //   const data = await fetch(
-  //     `https://api.legiscan.com/?key=${legiscanKey}&op=getSearch&state=HI&query=UHERO+OR+"task+force"+OR+"economic+research+organization"+OR+"working+group"`,
-  //     { next: { revalidate: 86400 } }
-  //   );
-
-  //   const results = await data.json();
-  //   const bills = await results.searchresult;
-  //   // clean up response data, reformat to an array
-  //   const billsArray: BillType[] = Object.values(bills);
-  //   billsArray.pop();
-  //   console.log(billsArray.length);
-  //   return billsArray
-  //   for (const bill of billsArray) {
-  //     await handleBill(bill, newBills, updatedBills);
-  //   }
-  //   // return billsArray
-  //   console.log(newBills);
-  //   console.log(updatedBills);
-  // } catch (error) {
-  //   console.log(error);
-  // }
+  const newBills: NewBill[] = [];
+  const updatedBills: UpdatedBillFields[] = [];
 
   const billsArray = await handleLegiscanData();
 
-  // console.log(requestDate);
-  // console.log(billsArray);
   if (billsArray) {
     for (const bill of billsArray) {
       await handleBill(bill, newBills, updatedBills);
     }
   }
-  const currentBills = await getData();
 
+  const currentBills = await getData();
+  // console.log(currentBills);
   console.log(newBills);
   console.log(updatedBills);
-  // console.log(currentBills);
-  // const billsFromDB = await getBillsFromDB();
 
   return (
     <>
