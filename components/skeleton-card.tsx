efdae4773188697f4915ec8c61cf3f47375699d3
dangabrielle <hoangabrielle@gmail.com>
2025-02-14 16:38:49 -1000

    improve loading skeleton card

--- components/skeleton-card.tsx
+++ components/skeleton-card.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import { Skeleton } from "./ui/skeleton";
 const SkeletonCard = () => {
   return (
-    <div className="flex size-full flex-col space-y-3">
-      <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+    <div className="flex size-full flex-col mt-5 space-y-3">
+      <Skeleton className="h-[125px] max-w-screen-md w-screen rounded-xl" />
       <div className="space-y-2">
-        <Skeleton className="h-4 w-[250px]" />
-        <Skeleton className="h-4 w-[200px]" />
+        <Skeleton className="h-20 w-screen max-w-screen-md" />
+        <Skeleton className="h-8 w-screen max-w-screen-md" />
+        <Skeleton className="h-8 w-screen max-w-screen-md" />
       </div>
     </div>
   );
