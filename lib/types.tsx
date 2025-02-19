1104f91bffe9715c2260102ff7d00d0a39b8d571
Gabrielle <hoangabrielle@gmail.com>
2025-02-18 17:27:32 -1000

    organize types

---
+++ lib/types.tsx
@@ -0,0 +1,32 @@
+export type BillType = {
+  relevance: number;
+  state: string;
+  bill_number: string;
+  bill_id: number;
+  change_hash: string;
+  url: string;
+  text_url: string;
+  research_url: string;
+  last_action_date: string;
+  last_action: string;
+  title: string;
+  created_at: Date;
+  last_updated?: Date;
+};
+
+export type UpdatedBillFields = {
+  bill_id: number;
+  bill_number: string;
+  relevance?: string;
+  state?: string;
+  change_hash: string;
+  url: string;
+  text_url?: string;
+  research_url?: string;
+  last_action_date?: string;
+  last_action?: string;
+  title?: string;
+  updatedDate: Date;
+};
+
+export type NewBill = [bill_number: string, url: string];
