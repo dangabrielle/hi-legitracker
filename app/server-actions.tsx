5281264592a192035e29f63653778d581f68a7dd
dangabrielle <hoangabrielle@gmail.com>
2025-02-20 16:24:53 -1000

    optimize db queries

--- app/server-actions.tsx
+++ app/server-actions.tsx
@@ -1,88 +1,93 @@
 "use server";
 
 import { neon } from "@neondatabase/serverless";
-import { BillType, UpdatedBillFields, NewBill } from "@/lib/types";
+import { Bill, UpdatedBillFields, NewBill } from "@/lib/types";
 
 const legiscanKey = process.env.LEGISCAN_KEY;
 const sql = neon(`${process.env.DATABASE_URL}`);
 
-export async function getData() {
+export async function getData(): Promise<Bill[]> {
   try {
     const dbBills = await sql`SELECT * FROM bills`;
-    return dbBills as BillType[];
+    return dbBills as Bill[];
   } catch (error) {
     console.error("Error fetching bills from db", error);
     return [];
   }
 }
 
+// initial page load cross checks current db bills with fetched bills from API
+// adds new bills and updates existing bills if hash property changed
 export async function handleBill(
-  bill: BillType,
+  bills: Bill[],
   newBills: NewBill[],
   updatedBills: UpdatedBillFields[]
-) {
-  const {
-    bill_id,
-    bill_number,
-    change_hash,
-    last_action,
-    last_action_date,
-    relevance,
-    research_url,
-    state,
-    text_url,
-    title,
-    url,
-  } = bill;
-
+): Promise<void> {
   try {
     // check if bill exists in db
-    const billStatus = await checkIfExists(bill_id, bill_number);
-    console.log(billStatus);
+    const [existingBills, billSet] = await checkIfExists(bills);
+    // O(1) lookup operation
+    const existingBillsMap = new Map(
+      existingBills.map((bill) => [bill.bill_id, bill])
+    );
+
     // if exists, check if hash changed + update as necessary
     // else add it to the db
-    if (billStatus) {
-      const response =
-        await sql`SELECT * FROM bills WHERE (bill_number = ${bill_number}) AND bill_id = ${bill_id}`;
-      const dbBill = response[0];
-      const dB_hash = dbBill.change_hash;
+    for (const bill of bills) {
+      const {
+        bill_id,
+        bill_number,
+        change_hash,
+        last_action,
+        last_action_date,
+        relevance,
+        research_url,
+        state,
+        text_url,
+        title,
+        url,
+      } = bill;
 
-      if (change_hash !== dB_hash) {
-        // for debugging purposes, distinguishes what fields have changed
-        updatedBills.push({
-          bill_id: bill_id,
-          bill_number: bill_number,
-          relevance: relevance !== dbBill.relevance ? relevance.toString() : "",
+      if (!billSet.has(bill_id)) {
+        await sql`INSERT INTO bills (bill_id, bill_number, change_hash, last_action, last_action_date, relevance, research_url, state, text_url, title, url) VALUES (${bill_id}, ${bill_number}, ${change_hash}, ${last_action}, ${last_action_date}, ${relevance}, ${research_url}, ${state},  ${text_url}, ${title}, ${url})`;
+        newBills.push([bill_number, url]);
+      } else {
+        const dbBill = existingBillsMap.get(bill_id);
+        if (dbBill && dbBill?.change_hash !== change_hash) {
+          // for debugging purposes, distinguishes what fields have changed
+          updatedBills.push({
+            bill_id: bill_id,
+            bill_number: bill_number,
+            relevance:
+              relevance !== dbBill.relevance ? relevance.toString() : "",
 
-          change_hash: change_hash,
-          url: url,
-          text_url: text_url !== dbBill.text_url ? text_url : "",
-          research_url:
-            research_url !== dbBill.research_url ? research_url : "",
-          last_action_date:
-            last_action_date !== dbBill.last_action_date
-              ? last_action_date
-              : "",
-          last_action: last_action !== dbBill.last_action ? last_action : "",
-          title: title !== dbBill.title ? title : "",
-          updatedDate: new Date(),
-        });
-        // updates bill accordingly
-        await sql`UPDATE bills SET change_hash = ${change_hash}, last_action=${last_action}, last_action_date=${last_action_date}, relevance=${relevance}, research_url=${research_url}, state=${state}, text_url=${text_url}, title=${title}, url=${url}, last_updated=now() WHERE (bill_number = ${bill_number}) AND bill_id = ${bill_id}`;
-        console.log("hash changed");
+            change_hash: change_hash,
+            url: url,
+            text_url: text_url !== dbBill.text_url ? text_url : "",
+            research_url:
+              research_url !== dbBill.research_url ? research_url : "",
+            last_action_date:
+              last_action_date !== dbBill.last_action_date
+                ? last_action_date
+                : "",
+            last_action: last_action !== dbBill.last_action ? last_action : "",
+            title: title !== dbBill.title ? title : "",
+            updatedDate: new Date(),
+          });
+          // updates bill accordingly
+          await sql`UPDATE bills SET change_hash = ${change_hash}, last_action=${last_action}, last_action_date=${last_action_date}, relevance=${relevance}, research_url=${research_url}, state=${state}, text_url=${text_url}, title=${title}, url=${url}, last_updated=now() WHERE (bill_number = ${bill_number}) AND bill_id = ${bill_id}`;
+          console.log("hash changed");
+        }
       }
-    } else {
-      await sql`INSERT INTO bills (bill_id, bill_number, change_hash, last_action, last_action_date, relevance, research_url, state, text_url, title, url) VALUES (${bill_id}, ${bill_number}, ${change_hash}, ${last_action}, ${last_action_date}, ${relevance}, ${research_url}, ${state},  ${text_url}, ${title}, ${url})`;
-
-      newBills.push([bill_number, url]);
     }
   } catch (error) {
     console.error(error);
   }
 }
 
-export async function handleLegiscanData(queryString: string) {
+export async function handleLegiscanData(queryString: string): Promise<Bill[]> {
   try {
+    // fetch data every 24 hrs
     const data = await fetch(
       `https://api.legiscan.com/?key=${legiscanKey}&op=getSearch&state=HI&query=${queryString}`,
       { next: { revalidate: 86400 } }
@@ -94,7 +99,7 @@ export async function handleLegiscanData(queryString: string) {
       const bills = await results.searchresult;
 
       // clean up response data, reformat to an array
-      const billsArray: BillType[] = Object.values(bills);
+      const billsArray: Bill[] = Object.values(bills);
 
       billsArray.pop();
 
@@ -103,30 +108,43 @@ export async function handleLegiscanData(queryString: string) {
   } catch (error) {
     console.error("Error pulling from API", error);
   }
+  return [];
 }
 
-export async function checkIfExists(bill_id: number, bill_number: string) {
-  //   const { bill_id, bill_number } = bill;
+// checks if data from API exists in db and returns existing bills that match
+export async function checkIfExists(
+  bills: Bill[]
+): Promise<[Bill[], Set<number>]> {
+  const bill_IDs = bills.map((bill) => bill.bill_id);
   try {
-    const billStatus =
-      await sql`SELECT EXISTS (SELECT 1 FROM bills WHERE (bill_number = ${bill_number}) AND bill_id = ${bill_id}) AS exists;`;
-    return billStatus[0].exists;
+    const existingBills =
+      await sql`SELECT * FROM bills WHERE bill_id = ANY(${bill_IDs})`;
+
+    // Stores existing bill ID's in a set for lookup operations
+    const billSet = new Set(existingBills.map((bill) => bill.bill_id));
+    return [existingBills as Bill[], billSet];
   } catch (error) {
     console.error("error checking across db", error);
   }
+  return [[], new Set()];
 }
 
-export async function handleNewQuery(bill: BillType) {
+// called if user adjusts search terms
+export async function handleNewQuery(bills: Bill[]): Promise<Bill[]> {
   try {
-    const { bill_id, bill_number } = bill;
-    const billStatus = await checkIfExists(bill_id, bill_number);
-    if (billStatus) {
-      const response =
-        await sql`SELECT * FROM bills WHERE (bill_number = ${bill_number}) AND bill_id = ${bill_id}`;
-      const dbBill = response[0];
-      return dbBill;
-    } else return bill;
+    const [existingBills, billSet] = await checkIfExists(bills);
+
+    // if bill does not exist in db, add it to returning array
+    // UI will have the option to add specific bill to db if desired
+    for (const bill of bills) {
+      if (!billSet.has(bill.bill_id)) {
+        existingBills.push(bill);
+      }
+    }
+
+    return existingBills;
   } catch (error) {
     console.error("error checking across db", error);
   }
+  return [];
 }
