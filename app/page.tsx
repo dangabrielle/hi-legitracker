b78195a8347af1241b94677a1357e27300cc982d
Gabrielle <hoangabrielle@gmail.com>
2025-02-24 17:25:52 -1000

    text message updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import BillTracker from "./bill-tracker";
-import { getData, handleBill, handleLegiscanData } from "./server-actions";
+import {
+  getData,
+  handleBill,
+  handleLegiscanData,
+  createMessage,
+} from "./server-actions";
 import { UpdatedBillFields, NewBill } from "@/lib/types";
 import { queryTerms, handleQueryString } from "@/lib/utils";
 
@@ -15,6 +20,24 @@ export default async function Home() {
 
   const dbBills = await getData();
 
+  if (newBills.length > 0 || updatedBills.length > 0) {
+    let message = `Updates as of ${new Date().toDateString()}:\n\n`;
+    if (updatedBills.length > 0) {
+      updatedBills.forEach((bill) => {
+        const { title, bill_number, url } = bill;
+        message += `${title} (${bill_number}): ${url}\n\n`;
+      });
+    }
+
+    if (newBills.length > 0) {
+      message += "New Bills:\n";
+      newBills.forEach((bill) => {
+        const [bill_number, url, title] = bill;
+        message += `${title} (${bill_number}): ${url}\n\n`;
+      });
+    }
+    await createMessage(message);
+  }
   console.log(newBills);
   console.log(updatedBills);
 
